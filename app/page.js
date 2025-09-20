1140464f43e51fbebe79cc1a42b9c64daf8fab1d
Jeffrey Jones S <135623574+JeffreyJones2003@users.noreply.github.com>
2025-09-20 19:10:09 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
@@ -7,7 +5,10 @@ export default function Home() {
       <header className="flex justify-between items-center py-4 px-6 bg-gray-800 shadow-md">
         <h1 className="text-xl font-bold text-white">Jeffrey Jones S</h1>
 
-     <a id="cta" href="/portfolio.png"   target="_blank" className="inline-block px-6 sm:px-8 py-2 sm:py-3 bg-blue-600 text-white font-semibold rounded hover:scale-105 transition-transform duration-200">
+     <a id="cta" href="/portfolio.png"   
+        target="_blank" 
+        className="inline-block px-6 sm:px-8 py-2 sm:py-3 bg-blue-600 text-white font-semibold rounded hover:scale-105 transition-transform duration-200"
+     >
           View Work
     </a>
       </header>
@@ -33,7 +34,6 @@ export default function Home() {
           </a>
         </div>
 
-        {/* Image Column */}
         <div className="md:w-1/2 relative z-10 mt-6 md:mt-0">
           <img
             src="/portfolio.png"
@@ -43,7 +43,6 @@ export default function Home() {
         </div>
       </main>
 
-      {/* Footer */}
       <footer className="text-center py-6 text-gray-400 text-sm">
         © 2025 Jeffrey Jones S
       </footer>
